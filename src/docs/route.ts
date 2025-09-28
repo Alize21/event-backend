145cdc91dc65b2b9508fe238ccb054b071c4387d
Alize21 <pusikentut@gmail.com>
2025-09-28 09:09:05 +0700

    fix(api-docs): use CDN to fixed swagger blank ui in production

--- src/docs/route.ts
+++ src/docs/route.ts
@@ -3,5 +3,12 @@ import swaggerUi from "swagger-ui-express";
 import swaggerOutput from "./swagger_output.json";
 
 export default function docs(app: Express) {
-  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerOutput));
+  app.use(
+    "/api-docs",
+    swaggerUi.serve,
+    swaggerUi.setup(swaggerOutput, {
+      customCssUrl: "https://cdn.jsdelivr.net/npm/swagger-ui-dist/swagger-ui.css",
+      customJs: ["https://cdn.jsdelivr.net/npm/swagger-ui-dist/swagger-ui-bundle.js", "https://cdn.jsdelivr.net/npm/swagger-ui-dist/swagger-ui-standalone-preset.js"],
+    })
+  );
 }
